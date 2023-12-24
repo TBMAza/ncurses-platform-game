fce0670e50020c7f0aa2b091c1eba999941faa8e
TBMAza <120518029+TBMAza@users.noreply.github.com>
2023-12-24 20:01:10 +0100

    Update Runner.cpp
    Added changedir() method to Runner class: if a Runner class enemy reaches the edge of a pit in the map, he changes direction to not fall

--- cppgame/src/characters/subclasses/enemies/subclasses/Runner.cpp
+++ cppgame/src/characters/subclasses/enemies/subclasses/Runner.cpp
@@ -8,4 +8,10 @@ Runner::Runner()
 Runner::Runner(int y, int x)
 {
     Character::init(RUNNER, ENEMY_INIT_HLT, y, x, LEFT, RUNNER_DMG);
-}
+};
+
+void Runner::changedir()
+{
+    if(this->direction == RIGHT) this->direction = LEFT;
+    else if(this->direction == LEFT) this->direction = RIGHT;
+};
