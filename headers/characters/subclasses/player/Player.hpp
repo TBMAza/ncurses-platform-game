9a38105fbb89e347b5748406236da52582f4930e
Alfahad Asaduz <120518029+TBMAza@users.noreply.github.com>
2023-12-26 21:26:29 +0100

    Update Player.hpp

--- headers/characters/subclasses/player/Player.hpp
+++ headers/characters/subclasses/player/Player.hpp
@@ -2,16 +2,24 @@
 #define PLAYER_HPP
 
 #include <ncurses.h>
+#include <math.h>
 #include "../../Character.hpp"
+#include "../../../../CollisionDetector.hpp"
 
 class Player: public Character
 {
     private:
 
         int cash;
+        
         chtype active_powerup; // power-up currently used by the Player
         chtype active_projectile; // projectile class currently used by the Player
-        int forcefield_qty, invisibility_qty; // quantity of forcefield and invisibility power-ups available in Player's inventory
+        
+        int forcefield_qty; // quantity of forcefield power-ups available in Player's inventory
+        int invisibility_qty; // quantity of invisibility power-ups available in Player's inventory
+        
+        short jumpstate; // member that keeps track if the player is currently jumping or not
+        short fallstate; // member that keeps track if the player is currently falling or not
     
     public:
 
@@ -23,6 +31,8 @@ class Player: public Character
         chtype getactivepowerup();
         chtype getactiveproj();
         int getffqty(); // returns the amount of forcefield power-ups owned by the Player
+        short getjumpstate();
+        short getfallstate();
 
         void setaspect(chtype asp);
         void incrcash(); // increments the amount of cash by 1 when the Player collects it in the map
@@ -30,8 +40,10 @@ class Player: public Character
         void setproj(chtype proj); // sets the active projectile, i.e. the projectile class that the player can currently use
         void incrforcefield(); // increments the amount of forcefield power-ups in the Player's inventory
         void incrinv(); // increments the amount of invisibility power-ups in the Player's inventory
+        void setjumpstate(short js);
         
         void jump();
+        void fall();
         chtype shoot(); // returns the active projectile; the damage that a character takes if hit by this shot will be calculated based on the projectile class, as all the classes of projectiles are mapped to their respective damage values in the constants.hpp file
 };
 
