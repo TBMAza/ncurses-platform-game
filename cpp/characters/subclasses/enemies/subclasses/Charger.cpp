900da98df535ad4df42f8ee9357ee331271537e8
Alfahad Asaduz <120518029+TBMAza@users.noreply.github.com>
2024-02-15 19:28:44 +0100

    Delete cpp directory

--- cpp/characters/subclasses/enemies/subclasses/Charger.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../../../../headers/characters/subclasses/enemies/subclasses/Charger.hpp"
-
-Charger::Charger(int y, int x)
-{
-    Character::init(CHARGER, ENEMY_INIT_HLT, y, x, LEFT, CHARGER_DMG);
-};
